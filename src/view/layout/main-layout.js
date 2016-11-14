edeed3f4f0e1c75b6acd870ad94ea46a7e4cef35
lijunyi01 <lijunyi01@gmail.com>
2016-11-14 09:37:09 +0800

    modify

--- src/view/layout/main-layout.js
+++ src/view/layout/main-layout.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import { Link,IndexLink } from 'react-router';
 import styles from '../styles/LayoutAndHome.css';
+import appleimage from '../images/apple.png';
 
 // Using "Stateless Functional Components"
 export default function(props) {
@@ -13,6 +14,8 @@ export default function(props) {
                 {/*<header className={styles.primaryheader}>www.myhis.com</header>*/}
                 <aside className={styles.primaryaside}>
                     <ul>
+                        <li><img src={appleimage} alt=""/></li>
+                        <li><p>&nbsp;</p></li>
                         <li><IndexLink to="/" activeClassName={styles.active}>Home</IndexLink></li>
                         <li><Link to="/myHisList" activeClassName={styles.active}>MyHisList</Link></li>
                     </ul>
